3d4f5c615533dab5818d44df20b90d9d29473408
Nurbek <nurbekizbassar@gmail.com>
2018-12-10 01:56:57 +0500

    Apollo and apollo-yoga  grapghql setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+
+const server = createServer();
+
+// TODO Use express middlware to handle cokkeies (JWT)
+// TODO Use express middlware to populate current user
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    }
+}, deets => {
+    console.log(`Server is now running on port ${deets.port}`)
+})
